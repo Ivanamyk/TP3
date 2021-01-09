a88e0d3f8bcbd8da1d4cd874b41e761e5eb71be5
Giuliana Cippitelli <giulianacippitelli@gmail.com>
2021-01-09 18:23:43 -0300

    boton editar/eliminar en tabla

--- index.js
+++ index.js
@@ -12,14 +12,32 @@ const createTable = (data) => {
     const tbody = document.getElementById("tbody");
     tbody.innerHTML = "";
     for (let object in data) {
-
         const tr = document.createElement('tr');
-
         for (let item in data[object]) {
             const td = document.createElement('td');
             td.innerHTML = data[object][item];
             tr.appendChild(td);
-        }
-        
+        }        
+        const botonEliminar = document.createElement('button');
+        botonEliminar.addEventListener('click', () => {
+            eliminar(object);
+        });
+        botonEliminar.innerText = 'Eliminar';
+        botonEliminar.setAttribute('class', 'btn btn-danger');
+        const tdElim = document.createElement('td');
+        tdElim.appendChild(botonEliminar);
+        tr.appendChild(tdElim);
+
+        const botonEditar = document.createElement('button');
+        botonEditar.addEventListener('click', () => {
+            window.location = `form.html?name=${object}`;
+        });
+        botonEditar.innerText = 'Editar';
+        botonEditar.setAttribute('class', 'btn btn-warning');
+        const tdEdit = document.createElement('td');
+        tdEdit.appendChild(botonEditar);
+        tr.appendChild(tdEdit);
+
+        tbody.appendChild(tr);
     }
 }
