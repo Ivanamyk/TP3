df82474c7e44eda9d2083a20f278b8cd32bc1500
Giuliana Cippitelli <giulianacippitelli@gmail.com>
2021-01-11 22:00:25 -0300

    Agrego modal edit y funcion editar user

--- index.js
+++ index.js
@@ -32,16 +32,20 @@ const createTable = (data) => {
         tr.appendChild(tdElim);
 
         const botonEditar = document.createElement('button');
-        botonEditar.addEventListener('click', () => {
-            window.location = `form.html?name=${object}`;
-        });
         botonEditar.innerText = 'Editar';
         botonEditar.setAttribute('class', 'btn btn-warning');
+        botonEditar.setAttribute('data-bs-toggle', 'modal');
+        botonEditar.setAttribute('data-bs-target', '#ModalEditar');
         const tdEdit = document.createElement('td');
         tdEdit.appendChild(botonEditar);
         tr.appendChild(tdEdit);
 
         tbody.appendChild(tr);
+
+        const editUserButton = document.getElementById("saveUserEdit");
+        editUserButton.addEventListener('click', () => {
+            editUser(object);
+        });
         
     }
 }
@@ -52,6 +56,11 @@ const inputName = document.getElementById("fullName");
 const inputEmail = document.getElementById("email");
 const inputAddress = document.getElementById("address");
 const inputPhone = document.getElementById("phone"); 
+
+const inputNameEdit = document.getElementById("fullNameEdit");
+const inputEmailEdit = document.getElementById("emailEdit");
+const inputAddressEdit = document.getElementById("addressEdit");
+const inputPhoneEdit = document.getElementById("phoneEdit"); 
 
 
 const addUser = (event) => {
@@ -77,24 +86,11 @@ const addUser = (event) => {
     })
 }
 
-const cargarForm = (object) => {
-    fetch(`${url}/users${object}.json`)
-        .then((response) => {
-            return response.json()
-        }).then((data) => {
-            console.log(data);
-            inputName.value = data.name
-            inputEmail.value = data.email
-            inputAddress.value = data.address
-            inputPhone.value = data.phone
-        })
-}
-
 const reload =() => {
     reload = location.reload();
 }
 
- const eliminar = async(object) => {
+const eliminar = async(object) => {
     await fetch(`${url}/users/${object}.json`, {
         method: 'DELETE',      
             })
@@ -103,10 +99,32 @@ const reload =() => {
             })
             .catch(error => console.log(error))
             reload()
-         }
+}
 
+const editUser = (object) => {
 
+    const editUser = {
+        fullName: inputNameEdit.value,
+        email: inputEmailEdit.value,
+        address: inputAddressEdit.value,
+        phone: inputPhoneEdit.value
+    }
+
+    fetch(`${url}/users/${object}.json`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'Application/json'
+        },
+        body: JSON.stringify(editUser),
+    }).then((response) => {
+        return response.json()
+    }).then((data) => {
+        console.log(data)
+    })
+}
     
 const addUserButton = document.getElementById("saveUser");
 addUserButton.addEventListener('click', addUser);
+
+
 
