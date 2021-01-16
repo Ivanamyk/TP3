ab35925f9f8854728fe8fc69ba3c2c0baa0d6eff
Ivana Yegros Kubicka <ivanamyk@gmail.com>
2021-01-16 19:01:13 -0300

    validacion modal y algunos arreglos mas

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const url = "https://trabajopractico3-6f33f-default-rtdb.firebaseio.com";
-// const params = new URLSearchParams(window.location.search)
-// const id = params.get('name');
+const params = new URLSearchParams(window.location.search);
+const id = params.get('name');
 
-
+//Cargo tabla
 const init = () => {
     fetch(`${url}/users.json`)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            console.log(data);
             createTable(data);
         })
-        .catch(error => console.log(error))
 };
 init();
 
@@ -33,18 +32,35 @@ const createTable = (data) => {
         tr.appendChild(tdAddress);
         tr.appendChild(tdPhone);
 
+        //Boton Eliminar
         const botonEliminar = document.createElement('button');
-        botonEliminar.addEventListener('click', () => {
-            eliminar(object);
-        });
-        botonEliminar.innerText = 'Eliminar';
+        botonEliminar.innerText = 'Delete';
         botonEliminar.setAttribute('class', 'btn btn-danger');
+        botonEliminar.setAttribute('data-bs-toggle', 'modal');
+        botonEliminar.setAttribute('data-bs-target', '#modalBorrar');
         const tdElim = document.createElement('td');
         tdElim.appendChild(botonEliminar);
         tr.appendChild(tdElim);
 
+        const deleteUserButton = document.getElementById("deleteUser");
+        deleteUserButton.addEventListener('click', () => {
+            eliminar(object);
+        });
+
+        // const botonEliminar = document.createElement('button');
+        // botonEliminar.addEventListener('click', () => {
+        //     eliminar(object);
+        // });
+        // botonEliminar.innerText = 'Eliminar';
+        // botonEliminar.setAttribute('class', 'btn btn-danger');
+        // const tdElim = document.createElement('td');
+        // tdElim.appendChild(botonEliminar);
+        // tr.appendChild(tdElim);
+
+
+        //Boton Editar
         const botonEditar = document.createElement('button');
-        botonEditar.innerText = 'Editar';
+        botonEditar.innerText = 'Edit';
         botonEditar.setAttribute('class', 'btn btn-warning');
         botonEditar.setAttribute('data-bs-toggle', 'modal');
         botonEditar.setAttribute('data-bs-target', '#ModalEditar');
@@ -58,13 +74,11 @@ const createTable = (data) => {
             traerDatos(object);
             const editUserButton = document.getElementById("saveUserEdit");
             editUserButton.addEventListener('click', () => {
-            editUser(object);
-
+                editUser(object);
+            });
         });
-        });  
     }
 }
-
 
 // MODAL
 const inputName = document.getElementById("fullName");
@@ -77,33 +91,66 @@ const inputEmailEdit = document.getElementById("emailEdit");
 const inputAddressEdit = document.getElementById("addressEdit");
 const inputPhoneEdit = document.getElementById("phoneEdit");
 
+//Validacion en Modal
+const validacion = () => {
+    const fullName = document.getElementById('fullName').value;
+    const email = document.getElementById('email').value;
+    const address = document.getElementById('address').value;
+    const phone = document.getElementById('phone').value;
+    const regExEmail = /\w+@\w+\.+(com)$/;
+    const regExPhone = /^[0-9\s-]+$/;
+
+    if (fullName.lenght > 50) {
+        alert('El Nombre debe contener hasta 50 caracteres.')
+        return false;
+    }
+    if (!regExEmail.test(email)) {
+        alert('Email invalido. Intente de nuevo.')
+        return false;
+    }
+    if (address.lenght > 60) {
+        alert('La Direccion debe contener hasta 60 caracteres.')
+        return false;
+    }
+    if (!regExPhone.test(phone)) {
+        alert('El Telefono debe contener solo números, espacios o guión medio')
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+//Funcion Añadir Usuario
 const addUser = (event) => {
     event.preventDefault();
+    //paso la validacion antes de hacer el POST asi corroboro los datos.
+    if (validacion()) {
+        const newUser = {
+            fullName: inputName.value,
+            email: inputEmail.value,
+            address: inputAddress.value,
+            phone: inputPhone.value
+        }
 
-    const newUser = {
-        fullName: inputName.value,
-        email: inputEmail.value,
-        address: inputAddress.value,
-        phone: inputPhone.value
+        fetch(`${url}/users.json`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'Application/json'
+            },
+            body: JSON.stringify(newUser),
+        }).then((response) => {
+            return response.json()
+        }).then((data) => {
+            console.log(data)
+        }).then(init);
     }
-
-    fetch(`${url}/users.json`, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'Application/json'
-        },
-        body: JSON.stringify(newUser),
-    }).then((response) => {
-        return response.json()
-    }).then((data) => {
-        console.log(data)
-    })
 }
 
 const reload = () => {
     reload = location.reload();
 }
-
+//Funcion Borrar Usuario
 const eliminar = async (object) => {
     await fetch(`${url}/users/${object}.json`, {
         method: 'DELETE',
@@ -115,8 +162,8 @@ const eliminar = async (object) => {
     reload()
 }
 
+//Funcion Editar
 const editUser = (object) => {
-
     const editUser = {
         fullName: inputNameEdit.value,
         email: inputEmailEdit.value,
@@ -134,10 +181,10 @@ const editUser = (object) => {
         return response.json()
     }).then((data) => {
         console.log(data)
-    })
+    }).then(init, location.reload());
 }
-//Recarga modal al editar 
 
+//Recarga modal al editar 
 const traerDatos = (object) => {
     fetch(`${url}/users/${object}.json`, {
         method: "GET",
@@ -157,6 +204,4 @@ const traerDatos = (object) => {
 
 const addUserButton = document.getElementById("saveUser");
 addUserButton.addEventListener('click', addUser);
-
-
 
